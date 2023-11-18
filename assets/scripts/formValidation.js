441ffdde1c6be3556422bec889dc3388e8d64f60
KejsiCarkanji <kcarkanji21@epoka.edu.al>
2023-11-18 15:37:30 +0100

    using local storage to save and retrieve data

--- assets/scripts/formValidation.js
+++ assets/scripts/formValidation.js
@@ -15,8 +15,8 @@ function validateAndSubmit(event){
     const surname = $("#surname").val();
     const email = $("#email").val();
     const phoneNumber = $("#phone").val();
-    const noAdults = $("#adults").val();
-    const noKids = $("#kids").val();
+    const noAdults = parseInt($("#adults").val(),10);
+    const noKids = parseInt($("#kids").val());
 
 
     if(name.length < 2){
@@ -38,6 +38,7 @@ function validateAndSubmit(event){
     const phoneRegex = /^[0-9]{10}$/;
         if (!phoneRegex.test(phoneNumber)) {
             $("#phoneSpn").html("Enter 10-digit phone number");
+            isValidated = false;
         }
 
     if(noAdults < 1){
@@ -55,6 +56,31 @@ function validateAndSubmit(event){
         return 
     }
 
+    handleSubmit(name, surname, email);
+}
+
+function handleSubmit(_name, _surname, _email){
+    // Create Object
+    var newBooking = {
+        id: Math.floor(Math.random()*1000),
+        name: _name,
+        surname: _surname,
+        email: _email
+    }
+
+    console.log('newBooking Object = ', newBooking);
+
+    //Retreive items from localstorage
+    var bookings = JSON.parse(localStorage.getItem('bookings')) || []; // if no items 
+    //in localstorage, return empty array
+
+    //Add order to existingOrders array
+    bookings.push(newBooking);
+
+    //Update localstorage with new items
+    localStorage.setItem('bookings', JSON.stringify(bookings)); //strngify e kthen ne string prap
+
+    alert('Order added to localstorage'); 
 }
 
 $(document).ready(function(){
