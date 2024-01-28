d3b065171e75cd0ab4a34cf133b61003d1656a59
KejsiCarkanji <kcarkanji21@epoka.edu.al>
2024-01-28 22:14:39 +0100

    delete update

--- assets/scripts/bookings.js
+++ assets/scripts/bookings.js
@@ -24,8 +24,9 @@ $(document).ready(function () {
     // Function to create booking cards
     function createBookingCard() {
         $.each(bookings, function(index, booking){
+            console.log('booking = ', booking);
                 const bookingCard = `
-                <div class="booking-card">
+                <div data-booking-id="${booking.id}" class="booking-card">
                     <p>Name: ${booking.name}</p>
                     <p>Surname: ${booking.surname}</p>
                     <p>Date: ${booking.bookingDate}</p>
@@ -35,21 +36,24 @@ $(document).ready(function () {
                     <p>Phone number: ${booking.phoneNumber}</p>
                     <p>Number of adults: ${booking.numberOfAdults}</p>
                     <p>Number of kids: ${booking.numberOfKids}</p>
-                    <button class="delete-booking" data-booking-id="${index}">Delete Booking</button>
+                    <button class="delete-booking">Delete Booking</button>
                 </div>`;
                 bookingsContainer.append(bookingCard);
             });
     }
 
     // Function to update the view after deleting a booking
-       function updateView() {
+       function updateView(bookingId) {
           console.log("updateView called");
-          bookingsContainer.empty();
-          createBookingCard();
+          console.log("bookingId = ", bookingId);
+          /*bookingsContainer.empty();
+          createBookingCard(); */
+
     }
 
     $(bookingsContainer).on('click', ".delete-booking", function () {
-        const bookingId = $(this).data('booking-id');
+        //const bookingId = $(this).data('booking-id');
+        const bookingId = $(this).closest('.booking-card').data('booking-id');
         console.log("bookingId = ", bookingId);
 
         // Make a DELETE request to the backend API to delete the booking
@@ -64,22 +68,11 @@ $(document).ready(function () {
         };
 
         $.ajax(deleteSettings).done(function () {
-            alert('Booking deleted successfully!');
+            alert('Booking deleted successfully!', bookingId);
             console.log("Before updateView");
             location.reload();
-            updateView(); // Update the view after successful deletion
+            updateView(bookingId); // Update the view after successful deletion
             console.log("after updateView");
         });
     });
 });
-
-
-    
-/*
-    // Event listener for delete button
-    bookingsContainer.on('click', '.delete-booking', function() {
-        const bookingIndex = $(this).data('id');
-        bookings.splice(bookingIndex, 1);
-        localStorage.setItem('bookings', JSON.stringify(bookings));
-        updateView();
-    }); */
